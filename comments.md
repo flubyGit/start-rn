e6090234c8cb3950a4c486390834db6a91b1d17f
Felipe <profissionalf.andre@gmail.com>
2020-09-16 19:10:15 -0300

    feat: first commit

---
+++ comments.md
@@ -0,0 +1,2 @@
+SafeAreaView - Utiliza um espaço para os componentes no ios na area segura.
+Text - Tag para string [p, abbr, span, h1-h6, etc]
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function App() {
+  // const jsx = <Text>Primeiro componente</Text>;
+  // return jsx;
+  return <Text>First component</Text>;
+}
